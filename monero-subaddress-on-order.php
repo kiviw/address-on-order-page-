e4239c9dc583b95a2bab26543da171ad0b19492f
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-21 10:30:23 +0300

    Create monero-subaddress-on-order.php

---
+++ monero-subaddress-on-order.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Plugin Name: Monero Subaddress on Order
+ * Description: Generates a Monero subaddress for each order.
+ * Version: 1.0
+ * Author: Your Name
+ */
+
+function generate_monero_subaddress_on_order($order_id) {
+    $rpc_url = 'http://127.0.0.1:18080/json_rpc'; // Replace with your Monero RPC URL
+
+    $request_body = json_encode([
+        'jsonrpc' => '2.0',
+        'id' => '0',
+        'method' => 'create_address',
+        'params' => [
+            'count' => 1,
+        ],
+    ]);
+
+    // Log the request for debugging
+    error_log('Monero RPC Request for Order ' . $order_id . ': ' . $request_body);
+
+    $response = wp_remote_post($rpc_url, [
+        'body' => $request_body,
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ],
+    ]);
+
+    if (!is_wp_error($response)) {
+        $body = wp_remote_retrieve_body($response);
+
+        // Log the response for debugging
+        error_log('Monero RPC Response for Order ' . $order_id . ': ' . $body);
+
+        $result = json_decode($body, true);
+
+        if (isset($result['result']['address'])) {
+            // Update the order with the generated subaddress
+            update_post_meta($order_id, '_monero_subaddress', $result['result']['address']);
+        }
+    }
+}
+
+add_action('woocommerce_new_order', 'generate_monero_subaddress_on_order', 10, 1);
+
+// Display the subaddress on the checkout page
+add_action('woocommerce_before_checkout_form', 'display_monero_subaddress_on_checkout');
+
+function display_monero_subaddress_on_checkout() {
+    $order_id = wc_get_checkout_order_received_id();
+    $subaddress = get_post_meta($order_id, '_monero_subaddress', true);
+
+    if (!empty($subaddress)) {
+        echo '<p><strong>Monero Subaddress:</strong> ' . esc_html($subaddress) . '</p>';
+    }
+}
